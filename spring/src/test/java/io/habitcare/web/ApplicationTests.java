3a36025db15b6bdd3f337b055f840eabfe726fe9
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-03-18 22:32:29 +0100

    Initial commit

---
+++ spring/src/test/java/io/habitcare/web/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.habitcare.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
