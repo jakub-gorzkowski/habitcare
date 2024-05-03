f382a39574b1909441f59003b899bd5ca0c7eedd
Hojniak Dominika <skubik1411@gmail.com>
2024-05-03 12:22:36 +0200

    dashboard and profile update

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import login from './pages/login';
+import Profile from './pages/Profile.js';
 import Dashboard from './pages/Dashboard.js';
 import Register from './pages/Register.js';
-import Profile from './pages/Profile.js';
+
 const App = () => {
   return (
       <Router>
