e696e8f78822384768f71672cc148e49d13392ab
Hojniak Dominika <skubik1411@gmail.com>
2024-05-03 12:24:56 +0200

    dashboard and profile update

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,18 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './pages/Profile.js';
 import Dashboard from './pages/Dashboard.js';
 import Register from './pages/Register.js';
-
 const App = () => {
-  return (
-      <Router>
-        <Routes>
-          {/*<Route path="/login" element={<Login />} />*/}
-            <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/*<Route path="/login" element={<Login />} />*/}
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
