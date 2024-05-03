5fb5ddb5ddae9ebc892ce60805f6aaf1db0f59d0
Kinga Furmanek <k.furmanek10@gmail.com>
2024-05-03 13:24:00 +0200

    Calendar page

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './pages/Profile.js';
 import Dashboard from './pages/Dashboard.js';
 import Register from './pages/Register.js';
+import Calendar from "./pages/Calendar.js"
+
 const App = () => {
     return (
         <Router>
@@ -11,6 +13,7 @@ const App = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/calendar" element={<Calendar />} />
             </Routes>
         </Router>
     );
