e6d8017fb8a12ae46fc6cbc8a8ae4481c8b0f17e
Szymon Jagla <szymon.jagla@student.pk.edu.pl>
2024-05-21 15:17:36 +0200

    handleSubmit method corrected

--- reactjs/src/components/DynamicForm.js
+++ reactjs/src/components/DynamicForm.js
@@ -1,11 +1,13 @@
-// DynamicForm.js
 import React from 'react';
 import axios from 'axios';
 import Input from './Input';
 import Button from './Button';
 import './DynamicForm.css';
+import { useHistory } from 'react-router-dom';
 
 const DynamicForm = ({ fields, buttonText, action }) => {
+    const history = useHistory();
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -16,8 +18,10 @@ const DynamicForm = ({ fields, buttonText, action }) => {
 
         const response = await axios.post(action, data);
 
-        if (response.data.accessToken) {
+        if (action.endsWith('/login') && response.data.accessToken) {
             localStorage.setItem('user', JSON.stringify(response.data));
+        } else if (action.endsWith('/register')) {
+            history.push('/login');
         }
     };
 
