94c26f8f36b93cc103c7350fdee1c3905915b69b
Kinga Furmanek <k.furmanek10@gmail.com>
2024-05-26 23:00:48 +0200

    HabitDetails endpoints added to front + score + check

--- reactjs/src/components/HabitItem.js
+++ reactjs/src/components/HabitItem.js
@@ -1,23 +1,54 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./HabitItem.css";
+import axios from "axios";
+import authHeader from "../service/auth-header";
 
-function HabitItem({ label, score, inviteText }) {
+function HabitItem({ label, inviteText, habitId, score, setScores }) {
     const [isChecked, setIsChecked] = useState(false);
 
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked);
+    const handleCheckboxChange = async () => {
+        try {
+            await axios.post(
+                `/api/habits/add-check/${habitId}`,
+                null,
+                {
+                    headers: authHeader(),
+                }
+            );
+            setIsChecked(!isChecked);
+            setScores(prevScores => ({
+                ...prevScores,
+                [habitId]: score + 1 
+            }));
+            alert("Check added successfully.");
+        } catch (error) {
+            console.error('There was an error while adding check:', error);
+        }
     };
+
+    const getDaysInMonth = () => {
+        const date = new Date();
+        return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    };
+
+    const scoreText = `${score}/${getDaysInMonth()}`;
+
     return (
         <div className="habit-item">
-            <h3 className="label">{label}</h3>
-            <div className="check"><input
-                type="checkbox"
-                className="checkbox"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-            /></div>
+            <Link to={`/habitdetails/${habitId}`} className="label-link">
+                <h3 className="label">{label}</h3>
+            </Link>
+            <div className="check">
+                <input
+                    type="checkbox"
+                    className="checkbox"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                />
+            </div>
             <div className="score">
-                Score: <span className="score-value">{score}</span>
+                Score: <span className="score-value">{scoreText}</span>
             </div>
             <div className="invite-text">{inviteText}</div>
         </div>
